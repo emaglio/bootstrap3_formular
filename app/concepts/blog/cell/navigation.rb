cf36f41112752f3094deeec4f27b13ee2d7f7b85
emaglio <emanuele.magliozzi@gmail.com>
2017-06-15 09:39:31 +1000

    Trying to fix the HTML for the navbar - button not shown (maybe write in XML and use html.safe to render it)

--- app/concepts/blog/cell/navigation.rb
+++ app/concepts/blog/cell/navigation.rb
@@ -13,6 +13,28 @@ module Blog::Cell
       return current_user != nil
     end
 
+    def main_nav_id
+      BootstrapVersion::Use == 3 ? "notId" : "mainNav"
+    end
+
+    def main_nav
+      BootstrapVersion::Use == 3 ? "navbar navbar-default navbar-custom navbar-fixed-top" : "navbar fixed-top navbar-toggleable-md navbar-light"
+    end
+
+    def container
+      BootstrapVersion::Use == 3 ? "container-fluid" : "container"
+    end
+
+    def navbar_header
+      BootstrapVersion::Use == 3 ? "<div class='navbar-header page-scroll'>" : " "
+    end
+
+    def menu_button
+      BootstrapVersion::Use == 3 ? "<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>" : "<button class='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>"
+    end
+
+
+
   end # class Navigation
 
 end # module Blog::Cell
